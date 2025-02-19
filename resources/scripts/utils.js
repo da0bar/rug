5038c203733093da2f83f924f515afe99fb44dd7
da0bar <da0bar@proton.me>
2025-02-19 16:12:42 -0500

    ui update

--- resources/scripts/utils.js
+++ resources/scripts/utils.js
@@ -51,7 +51,8 @@ function closeNotification(notificationCard) {
         notificationCard.remove();
     });
 }
-function updateWalletUI(account) {
+function updateWalletUI(account, connected = true) {
+    if (connected) {
     connectWalletButton.innerHTML = `
         <div>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -64,6 +65,19 @@ function updateWalletUI(account) {
     connectWalletButton.classList.add("connected");
 
     showNotification("Wallet connected!", 3000, "success");
+    } else {
+    connectWalletButton.innerHTML = `
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+            </svg>
+        </div>
+        <span>Connect Wallet</span>
+    `;
+    connectWalletButton.classList.remove("connected");
+
+    showNotification("Wallet disconnected", 3000, "success");
+    }
 }
 async function updateGameState(contract, forceUpdate = false) {
     const activePlayersCount = await contract.activePlayers();
