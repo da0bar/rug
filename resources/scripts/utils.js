6979963a29909493cdac210c3a3d26c31580603d
da0bar <da0bar@proton.me>
2025-02-19 16:26:16 -0500

    ui update

--- resources/scripts/utils.js
+++ resources/scripts/utils.js
@@ -53,32 +53,26 @@ function closeNotification(notificationCard) {
 }
 function updateWalletUI(account) {
     console.log('account', account);    
-    if (account) {
+    let text;
+    account ? (text = `account.slice(0, 6) + '...' + account.slice(-4)`) : (text = "Connect Wallet");
     connectWalletButton.innerHTML = `
         <div>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
             </svg>
         </div>
-        <span>${account.slice(0, 6)}...${account.slice(-4)}</span>
+        <span>${text}</span>
     `;
 
-    connectWalletButton.classList.add("connected");
-
-    showNotification("Wallet connected!", 3000, "success");
-    } else {
-    connectWalletButton.innerHTML = `
-        <div>
-           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
-            </svg>
-        </div>
-        <span>Connect Wallet</span>
-    `;
-    connectWalletButton.classList.remove("connected");
-
-    showNotification("Wallet disconnected", 3000, "success");
+    if (account) {
+        connectWalletButton.classList.add("connected");
+        showNotification("Wallet connected!", 3000, "success");
     }
+    else {
+        connectWalletButton.classList.remove("connected");
+        showNotification("Wallet disconnected", 3000, "success");
+    }
+  
 }
 async function updateGameState(contract, forceUpdate = false) {
     const activePlayersCount = await contract.activePlayers();
