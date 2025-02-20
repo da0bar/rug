b80573563948f75bb9a0a1ec52eb13224eabb76a
da0bar <da0bar@proton.me>
2025-02-19 21:39:53 -0500

    ui update

--- resources/effect/matrix.js
+++ resources/effect/matrix.js
@@ -11,6 +11,8 @@ const timer = {
             const currentTime = Math.floor(Date.now() / 1000);
             const elapsedTime = currentTime - roundStart;
             const timeLeft = ROUND_DURATION - elapsedTime;
+            console.log(currentTime)
+            console.log(roundStart)
 
             const playerLight = document.getElementById("player-light");
             const status = document.getElementById("status");
